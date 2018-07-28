c6b2d10656f6dcfcf6996889985e963a5213ff12
Peanut <18915000292@163.com>
2018-07-28 10:31:04 +0800

    完成静态

---
+++ src/components/msgdetail.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "msgdetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/userdetail.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "userdetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
